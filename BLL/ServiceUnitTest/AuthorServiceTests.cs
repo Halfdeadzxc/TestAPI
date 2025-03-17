9b6854b5501ce52e4c7e77aa3a4762b7b007c942
dead <winter34577@gmail.com>
2025-03-17 15:25:25 +0300

    1

---
+++ BLL/ServiceUnitTest/AuthorServiceTests.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BLL.DTO;
+using BLL.Services;
+using BLL.Profiles;
+using DAL.Interfaces;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using DAL;
+using DAL.EFRepositories;
+namespace UnitTests
+{
+    public class AuthorServiceTests
+    {
+        private readonly IMapper _mapper;
+
+        public AuthorServiceTests()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Author, AuthorDTO>().ReverseMap();
+            });
+
+            _mapper = config.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_ReturnsCorrectAuthor()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "AuthorTestDb")
+                .Options;
+
+            using (var context = new AppDbContext(options))
+            {
+                context.Authors.Add(new Author { Id = 1, FirstName = "John", LastName = "Doe", BirthDate = new DateTime(1970, 1, 1), Country = "USA" });
+                context.SaveChanges();
+
+                var repository = new EFAuthorRepository(context);
+                var service = new AuthorService(repository, _mapper);
+
+                var author = await service.GetByIdAsync(1);
+
+                Assert.NotNull(author);
+                Assert.Equal("John", author.FirstName);
+                Assert.Equal("Doe", author.LastName);
+            }
+        }
+
+        [Fact]
+        public async Task AddAsync_AddsNewAuthor()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "AuthorTestDb2")
+                .Options;
+
+            using (var context = new AppDbContext(options))
+            {
+                var repository = new EFAuthorRepository(context);
+                var service = new AuthorService(repository, _mapper);
+
+                var newAuthor = new AuthorDTO
+                {
+                    FirstName = "Jane",
+                    LastName = "Smith",
+                    BirthDate = new DateTime(1990, 2, 2),
+                    Country = "UK"
+                };
+
+                await service.AddAsync(newAuthor);
+
+                var addedAuthor = context.Authors.FirstOrDefault();
+                Assert.NotNull(addedAuthor);
+                Assert.Equal("Jane", addedAuthor.FirstName);
+                Assert.Equal("Smith", addedAuthor.LastName);
+            }
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ReturnsAllAuthors()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "AuthorTestDb1")
+                .Options;
+
+            using (var context = new AppDbContext(options))
+            {
+                context.Authors.Add(new Author { Id = 1, FirstName = "John", LastName = "Doe", BirthDate = new DateTime(1970, 1, 1), Country = "USA" });
+                context.Authors.Add(new Author { Id = 2, FirstName = "Alice", LastName = "Brown", BirthDate = new DateTime(1985, 5, 10), Country = "Canada" });
+                context.SaveChanges();
+
+                var repository = new EFAuthorRepository(context);
+                var service = new AuthorService(repository, _mapper);
+
+                var authors = await service.GetAllAsync();
+
+                Assert.Equal(2, authors.Count());
+            }
+        }
+        [Fact]
+        public async Task UpdateAsync_UpdatesAuthorCorrectly()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "AuthorUpdateTestDb4")
+                .Options;
+
+            using (var context = new AppDbContext(options))
+            {
+                context.Authors.Add(new Author { Id = 1, FirstName = "John", LastName = "Doe", BirthDate = new DateTime(1970, 1, 1), Country = "USA" });
+                context.SaveChanges();
+
+                var repository = new EFAuthorRepository(context);
+                var service = new AuthorService(repository, _mapper);
+
+                var updatedAuthorDto = new AuthorDTO
+                {
+                    Id = 1,
+                    FirstName = "UpdatedJohn",
+                    LastName = "UpdatedDoe",
+                    BirthDate = new DateTime(1970, 1, 1),
+                    Country = "Canada"
+                };
+
+                await service.UpdateAsync(updatedAuthorDto);
+
+                var updatedAuthor = context.Authors.FirstOrDefault(a => a.Id == 1);
+                Assert.NotNull(updatedAuthor);
+                Assert.Equal("UpdatedJohn", updatedAuthor.FirstName);
+                Assert.Equal("UpdatedDoe", updatedAuthor.LastName);
+                Assert.Equal("Canada", updatedAuthor.Country);
+            }
+        }
+
+        [Fact]
+        public async Task DeleteAsync_RemovesAuthorCorrectly()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "AuthorDeleteTestDb5")
+                .Options;
+
+            using (var context = new AppDbContext(options))
+            {
+                context.Authors.Add(new Author { Id = 1, FirstName = "John", LastName = "Doe", BirthDate = new DateTime(1970, 1, 1), Country = "USA" });
+                context.SaveChanges();
+
+                var repository = new EFAuthorRepository(context);
+                var service = new AuthorService(repository, _mapper);
+
+                
+                await service.DeleteAsync(1);
+
+                
+                var deletedAuthor = context.Authors.FirstOrDefault(a => a.Id == 1);
+                Assert.Null(deletedAuthor); 
+            }
+        }
+
+    }
+
+
+
+}
---
+++ DAL/RepositoryUnitTest/EFAuthorRepositoryTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using DAL.EFRepositories;
+using DAL.Interfaces;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using DAL;
+
+namespace UnitTests
+{
+    public class EFAuthorRepositoryTests
+    {
+        private DbContextOptions<AppDbContext> CreateNewContextOptions(string databaseName)
+        {
+            return new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: databaseName)
+                .Options;
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_ReturnsCorrectAuthor()
+        {
+            var options = CreateNewContextOptions("GetByIdTestDb");
+
+            using (var context = new AppDbContext(options))
+            {
+                
+                var author = new Author { Id = 1, FirstName = "John", LastName = "Doe", BirthDate = new DateTime(1970, 1, 1), Country = "USA" };
+                context.Authors.Add(author);
+                context.SaveChanges();
+
+                var repository = new EFAuthorRepository(context);
+
+                var result = await repository.GetByIdAsync(1);
+
+                Assert.NotNull(result);
+                Assert.Equal("John", result.FirstName);
+                Assert.Equal("Doe", result.LastName);
+            }
+        }
+
+        [Fact]
+        public async Task AddAsync_AddsAuthorToDatabase()
+        {
+            var options = CreateNewContextOptions("AddTestDb2");
+
+            using (var context = new AppDbContext(options))
+            {
+                var repository = new EFAuthorRepository(context);
+                var newAuthor = new Author
+                {
+                    FirstName = "Jane",
+                    LastName = "Smith",
+                    BirthDate = new DateTime(1990, 2, 2),
+                    Country = "UK"
+                };
+
+                await repository.AddAsync(newAuthor);
+
+                var authorInDb = context.Authors.FirstOrDefault(a => a.FirstName == "Jane" && a.LastName == "Smith");
+                Assert.NotNull(authorInDb);
+                Assert.Equal("Jane", authorInDb.FirstName);
+                Assert.Equal("Smith", authorInDb.LastName);
+                Assert.Equal("UK", authorInDb.Country);
+            }
+        }
+    }
+}
